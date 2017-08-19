3495ecdd3994f0b91567d873f4175662f186532c
Yusuke Shinada <yusuke19831022@gmail.com>
2017-08-19 20:29:31 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -12,7 +12,7 @@ function setup() {
 	var cnv = createCanvas(400, 300);
 	circle = new Circle();
 	weatherAsk = new WeatherAsk();
-	cnv.position(0, 300);
+	cnv.position(0, 500);
 	//setup the button, 1.Access to ID in html, 2. Connect the button to mousePressed().  
 	button1 = select('#button1');
 	button1.mousePressed(weatherAsk.ask1);
